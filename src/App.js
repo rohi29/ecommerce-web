67eb7a4ceec8df1247a39d93200306d2fbbeb554
rohini <rohiniphatangaren5@gmail.com>
2022-02-28 18:47:27 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Header from "./components/Header";
+import { BrowserRouter as Redirect, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+
+import Products from "./components/Products";
+import { DataProvider } from "./components/DataProvider";
+import Details from "./components/Details/Details";
+import Cart from "./components/Cart";
+import Register from "./components/Signup/Register";
+import Error from "./components/Error";
 
 function App() {
+
+  const [search, setSearch] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DataProvider>
+      <>
+        <Routes>
+          <Route exact path="/error404" element={<Error/>} />
+          <Route exact path="/header" element={<Header setSearch={setSearch} />}>
+
+              <Route exact path="/" element={<Home />} />
+              <Route exact path="/products" element={() => <Products search={search} />} />
+              <Route exact path="/products/:id" element={<Details />} />
+              <Route exact path="/cart" element={<Cart />} />
+              <Route exact path="/register" element={<Register />} />
+              <Redirect to="/error404" />
+              <Products />
+           
+            
+            
+          </Route>
+        </Routes>
+      </>
+    </DataProvider>
   );
 }
 
